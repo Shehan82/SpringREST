c56a256d09dd014258776ffdb67b7775aa358fff
shehan82 <shehansandeepa82@gmail.com>
2021-05-15 00:19:32 +0530

    initialize the project

---
+++ src/main/java/com/example/springRest/SpringRestWithFirebase/SpringRestWithFirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springRest.SpringRestWithFirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWithFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWithFirebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springRest/SpringRestWithFirebase/SpringRestWithFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRest.SpringRestWithFirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWithFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
