50718d313badb751df666fb5f6413441a1213caa
shehan82 <shehansandeepa82@gmail.com>
2021-05-15 01:12:53 +0530

    added serviceAccountKey to gitignore file and create Firebase initialization class for firebase connectivity

---
+++ src/main/java/com/example/springRest/SpringRestWithFirebase/FirebaseInitialization.java
@@ -0,0 +1,39 @@
+package com.example.springRest.SpringRestWithFirebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitialization {
+
+    public void initialization()  {
+
+
+        FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream("./serviceAccountKey.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://todo-app-58ecc.firebaseio.com")
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseApp.initializeApp(options);
+
+    }
+}
