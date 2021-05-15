23b07526a56924d72cf3dcf797d937b004d9d085
shehan82 <shehansandeepa82@gmail.com>
2021-05-15 10:03:59 +0530

    saveStudent function done

--- src/main/java/com/example/springRest/SpringRestWithFirebase/FirebaseInitialization.java
+++ src/main/java/com/example/springRest/SpringRestWithFirebase/FirebaseInitialization.java
@@ -5,13 +5,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Service
 public class FirebaseInitialization {
-
+    @PostConstruct
     public void initialization()  {
 
 
